4bc6e0d6f91634eff0ade2dfede475ddbc00343e
Chris Fitzpatrick <chrisfitzpat@gmail.com>
2019-03-22 11:16:24 +0100

    Add created_at and updated_at to Topic.as_json
    Add modifies to the Topic json serialization

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -2,11 +2,13 @@ class Topic < ApplicationRecord
   belongs_to :user, counter_cache: true
   validates :title, :body, presence: true
 
+  include SwaggableModel
+
   def author
     user.name
   end
 
-  def as_json(opts = { only: %i(id title body user_id), methods: :author })
+  def as_json(opts = { only: %i(id title body user_id created_at updated_at), methods: :author })
     super(opts)
   end
 end
